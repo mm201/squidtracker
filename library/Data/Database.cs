bc95fa6a75ecd7607c330d1a2d6648dad769a182
Greg Edwards <edwardsgreg@gmail.com>
2015-07-11 00:35:33 -0400

    Made crawler work.

--- library/Data/Database.cs
+++ library/Data/Database.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using MySql.Data;
 using MySql.Data.MySqlClient;
-using Newtonsoft.Json;
 
 namespace SquidTracker.Data
 {
@@ -16,36 +15,88 @@ namespace SquidTracker.Data
             return new MySqlConnection(ConfigurationManager.ConnectionStrings["squidTrackerConnectionString"].ConnectionString);
         }
 
-        public static void LogStagesInfo(String data)
+        private static void WithConnection(Action<MySqlConnection> action)
         {
-            StagesInfoRecord[] records = null;
-            try
-            {
-                records = JsonConvert.DeserializeObject<StagesInfoRecord[]>(data);
-            }
-            catch { }
-
             using (MySqlConnection conn = CreateConnection())
             {
                 conn.Open();
-                conn.ExecuteNonQuery("INSERT INTO squid_logs_stages_info (date, data) " +
-                    "VALUES (GETUTCDATE(), @data)",
-                    new MySqlParameter("@data", data));
-
-                if (records.Length > 0)
-                {
-                    StagesInfoRecord record = records[0];
-                    foreach (RankingRecord rr in record.ranking)
-                    {
-                        // scrape all the identifiers we can
-                        if (rr.weapon_id != null) GetWeaponId(conn, rr.weapon_id);
-                        if (rr.gear_shoes_id != null) GetShoesId(conn, rr.gear_shoes_id);
-                        if (rr.gear_clothes_id != null) GetShirtId(conn, rr.gear_clothes_id);
-                        if (rr.gear_head_id != null) GetHatId(conn, rr.gear_head_id);
-                    }
-                }
+                action(conn);
                 conn.Close();
             }
+        }
+
+        private static TRet WithConnection<TRet>(Func<MySqlConnection, TRet> func)
+        {
+            using (MySqlConnection conn = CreateConnection())
+            {
+                conn.Open();
+                return func(conn);
+            }
+        }
+
+        public static void LogStagesInfo(MySqlConnection conn, String data)
+        {
+            conn.ExecuteNonQuery("INSERT INTO squid_logs_stages_info (date, data) " +
+                "VALUES (UTC_TIMESTAMP(), @data)",
+                new MySqlParameter("@data", data));
+        }
+
+        public static void LogStagesInfo(String data)
+        {
+            WithConnection(conn => LogStagesInfo(conn, data));
+        }
+
+        public static bool InsertLeaderboard(MySqlConnection conn, StagesInfoRecord leaderboard)
+        {
+            int count = Convert.ToInt32(DatabaseExtender.Cast<object>(conn.ExecuteScalar(
+                "SELECT Count(*) FROM squid_leaderboards WHERE term_begin = @term_begin", 
+                new MySqlParameter("@term_begin", leaderboard.datetime_term_begin))));
+            if (count > 0) return false;
+
+            uint? stage1 = null, stage2 = null, stage3 = null;
+            if (leaderboard.stages.Length > 0) stage1 = GetStageId(conn, leaderboard.stages[0]);
+            if (leaderboard.stages.Length > 1) stage2 = GetStageId(conn, leaderboard.stages[1]);
+            if (leaderboard.stages.Length > 2) stage3 = GetStageId(conn, leaderboard.stages[2]);
+
+            uint rowId = Convert.ToUInt32(DatabaseExtender.Cast<object>(conn.ExecuteScalar(
+                "INSERT INTO squid_leaderboards " +
+                "(term_begin, term_end, stage1_id, stage2_id, stage3_id) VALUES " +
+                "(@term_begin, @term_end, @stage1_id, @stage2_id, @stage3_id); SELECT LAST_INSERT_ID()",
+                new MySqlParameter("@term_begin", leaderboard.datetime_term_begin),
+                new MySqlParameter("@term_end", leaderboard.datetime_term_end),
+                new MySqlParameter("@stage1_id", stage1 ?? (object)DBNull.Value),
+                new MySqlParameter("@stage2_id", stage2 ?? (object)DBNull.Value),
+                new MySqlParameter("@stage3_id", stage3 ?? (object)DBNull.Value)
+                )));
+
+            for (int x = 0; x < leaderboard.ranking.Length; x++)
+            {
+                RankingRecord rr = leaderboard.ranking[x];
+                uint? weaponId = rr.weapon_id == null ? (uint?)null : GetWeaponId(conn, rr.weapon_id);
+                uint? shoesId = rr.gear_shoes_id == null ? (uint?)null : GetShoesId(conn, rr.gear_shoes_id);
+                uint? shirtId = rr.gear_clothes_id == null ? (uint?)null : GetShirtId(conn, rr.gear_clothes_id);
+                uint? hatId = rr.gear_head_id == null ? (uint?)null : GetHatId(conn, rr.gear_head_id);
+
+                conn.ExecuteNonQuery("INSERT INTO squid_leaderboard_entries " +
+                    "(leaderboard_id, position, mii_name, weapon_id, gear_shoes_id, " +
+                    "gear_clothes_id, gear_head_id) VALUES " +
+                    "(@leaderboard_id, @position, @mii_name, @weapon_id, @gear_shoes_id, " +
+                    "@gear_clothes_id, @gear_head_id)",
+                    new MySqlParameter("@leaderboard_id", rowId),
+                    new MySqlParameter("@position", x + 1),
+                    new MySqlParameter("@mii_name", rr.mii_name),
+                    new MySqlParameter("@weapon_id", weaponId),
+                    new MySqlParameter("@gear_shoes_id", shoesId),
+                    new MySqlParameter("@gear_clothes_id", shirtId),
+                    new MySqlParameter("@gear_head_id", hatId)
+                    );
+            }
+            return true;
+        }
+
+        public static bool InsertLeaderboard(StagesInfoRecord leaderboard)
+        {
+            return WithConnection(conn => InsertLeaderboard(conn, leaderboard));
         }
 
         private static uint GetAnyId(MySqlConnection conn, String table, String identifier, String name)
@@ -53,9 +104,10 @@ namespace SquidTracker.Data
             if (table == null) throw new ArgumentNullException();
             if (identifier == null) throw new ArgumentNullException();
 
-            object o = DatabaseExtender.Cast<object>(conn.ExecuteScalar("SELECT id FROM " + 
+            object o1 = conn.ExecuteScalar("SELECT id FROM " + 
                 table + " WHERE identifier = @identifier", 
-                new MySqlParameter("@identifier", identifier)));
+                new MySqlParameter("@identifier", identifier));
+            object o = DatabaseExtender.Cast<object>(o1);
             if (o != null) return Convert.ToUInt32(o);
 
             if (name == null)
@@ -74,27 +126,27 @@ namespace SquidTracker.Data
             }
         }
 
-        private static uint GetStageId(MySqlConnection conn, String identifier, String name)
+        public static uint GetStageId(MySqlConnection conn, StageRecord sr)
         {
-            return GetAnyId(conn, "squid_stages", identifier, name);
+            return GetAnyId(conn, "squid_stages", sr.id, sr.name);
         }
 
-        private static uint GetWeaponId(MySqlConnection conn, String identifier)
+        public static uint GetWeaponId(MySqlConnection conn, String identifier)
         {
             return GetAnyId(conn, "squid_weapons", identifier, null);
         }
 
-        private static uint GetShoesId(MySqlConnection conn, String identifier)
+        public static uint GetShoesId(MySqlConnection conn, String identifier)
         {
             return GetAnyId(conn, "squid_gear_shoes", identifier, null);
         }
 
-        private static uint GetShirtId(MySqlConnection conn, String identifier)
+        public static uint GetShirtId(MySqlConnection conn, String identifier)
         {
             return GetAnyId(conn, "squid_gear_clothes", identifier, null);
         }
 
-        private static uint GetHatId(MySqlConnection conn, String identifier)
+        public static uint GetHatId(MySqlConnection conn, String identifier)
         {
             return GetAnyId(conn, "squid_gear_head", identifier, null);
         }
--- splatcrawler/Service1.cs
+++ splatcrawler/Service1.cs
@@ -4,8 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using SquidTracker.Data;
 
 namespace splatcrawler
 {
@@ -23,8 +28,69 @@ namespace splatcrawler
 
         public void Start()
         {
-            Console.WriteLine("this works");
+            // todo: thread me
+            DateTime lastPollTime = DateTime.MinValue;
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+                if (lastPollTime.AddMinutes(10) < now)
+                {
+                    // 10 minutes passed, so poll
+                    lastPollTime = now;
+                    using (WebClient wc = new WebClient())
+                    {
+                        wc.Encoding = Encoding.UTF8;
+                        String data = wc.DownloadString("http://s3-ap-northeast-1.amazonaws.com/splatoon-data.nintendo.net/stages_info.json");
+                        ProcessStagesInfo(data);
+                    }
+                }
+                Thread.Sleep(1000);
+            }
         }
+
+        public static void ProcessStagesInfo(String data)
+        {
+            using (MySqlConnection conn = Database.CreateConnection())
+            {
+                conn.Open();
+
+                Database.LogStagesInfo(conn, data);
+
+                StagesInfoRecord[] records = null;
+                try
+                {
+                    records = JsonConvert.DeserializeObject<StagesInfoRecord[]>(data);
+                }
+                catch { }
+
+                if (records == null || records.Length == 0) return;
+
+                {
+                    StagesInfoRecord record = records[0];
+                    // scrape all the identifiers we can
+                    foreach (StageRecord sr in record.stages)
+                    {
+                        if (sr != null) Database.GetStageId(conn, sr);
+                    }
+                    foreach (RankingRecord rr in record.ranking)
+                    {
+                        if (rr.weapon_id != null) Database.GetWeaponId(conn, rr.weapon_id);
+                        if (rr.gear_shoes_id != null) Database.GetShoesId(conn, rr.gear_shoes_id);
+                        if (rr.gear_clothes_id != null) Database.GetShirtId(conn, rr.gear_clothes_id);
+                        if (rr.gear_head_id != null) Database.GetHatId(conn, rr.gear_head_id);
+                    }
+                }
+
+                for (int x = 1; x < records.Length; x++)
+                {
+                    Database.InsertLeaderboard(conn, records[x]);
+                    Console.WriteLine("Inserted leaderboard for {0} to {1}", records[x].datetime_term_begin, records[x].datetime_term_end);
+                }
+
+                conn.Close();
+            }
+        }
+
 
         protected override void OnStop()
         {
