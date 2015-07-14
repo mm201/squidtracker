11af9621d2fadea937f3c2964b3992ea76d8c50b
Greg Edwards <edwardsgreg@gmail.com>
2015-07-14 11:10:49 -0400

    Smarter polling, gets new stages within 5 seconds of being published.

--- splatcrawler/Service1.cs
+++ splatcrawler/Service1.cs
@@ -30,14 +30,15 @@ namespace splatcrawler
         {
             // todo: thread me
             DateTime nextPollTime = DateTime.MinValue;
+            PollTypes pollType = PollTypes.Initial;
+            bool freshShortUpdate = false;
             while (true)
             {
-                DateTime now = DateTime.Now;
+                DateTime now = DateTime.UtcNow;
                 if (nextPollTime < now)
                 {
                     // 30 minutes passed, so poll
-                    const int DELAY_SECONDS = 70;
-                    DateTime nextAccurate = now.AddMinutes(30).AddSeconds(-DELAY_SECONDS); // next poll time before rounding
+                    DateTime nextAccurate = now.AddMinutes(30); // next poll time before rounding
                     nextPollTime = new DateTime(
                         nextAccurate.Year,
                         nextAccurate.Month, 
@@ -45,35 +46,83 @@ namespace splatcrawler
                         nextAccurate.Hour,
                         nextAccurate.Minute >= 30 ? 30 : 0, 
                         0, 
-                        nextAccurate.Kind).AddSeconds(DELAY_SECONDS);
+                        nextAccurate.Kind);
 
+                    StagesInfoRecord[] records;
                     using (WebClient wc = new WebClient())
                     {
                         wc.Encoding = Encoding.UTF8;
                         String data = wc.DownloadString("http://s3-ap-northeast-1.amazonaws.com/splatoon-data.nintendo.net/stages_info.json");
-                        ProcessStagesInfo(data);
+                        records = ProcessStagesInfo(data, pollType);
+                    }
+
+                    pollType = PollTypes.Ambient;
+
+                    if (records.Length > 0)
+                    {
+                        const int PRE_EMPT = 10; // seconds before map rotation when we begin polling
+                        const int FAST_POLL_RATE = 5; // seconds between polls
+                        TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+                        StagesInfoRecord record = records[0];
+                        DateTime endTimeUtc = TimeZoneInfo.ConvertTimeToUtc(record.datetime_term_end, tzi).AddSeconds(-PRE_EMPT);
+                        if (endTimeUtc < now)
+                        {
+                            // received data is stale so we are polling
+                            nextPollTime = now.AddSeconds(FAST_POLL_RATE);
+                            if (freshShortUpdate)
+                                Console.Write(".");
+                            else
+                                Console.Write("Waiting for new maps.");
+                            pollType = PollTypes.Fresh;
+
+                            freshShortUpdate = true;
+                        }
+                        else if (endTimeUtc < nextPollTime)
+                        {
+                            // end of rotation comes sooner than ambient polling
+                            nextPollTime = endTimeUtc;
+                            Console.WriteLine("Polling for fresh maps at {0:G}.", nextPollTime.ToLocalTime());
+                            pollType = PollTypes.Fresh;
+                            freshShortUpdate = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Next poll at {0:G}.", nextPollTime.ToLocalTime());
+                            freshShortUpdate = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Next poll at {0:G}.", nextPollTime.ToLocalTime());
+                        freshShortUpdate = false;
                     }
                 }
                 Thread.Sleep(1000);
             }
         }
 
-        public static void ProcessStagesInfo(String data)
+        public static StagesInfoRecord[] GetStagesInfo(String data)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<StagesInfoRecord[]>(data);
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+
+        private static StagesInfoRecord[] ProcessStagesInfo(String data, PollTypes pollType)
         {
             using (MySqlConnection conn = Database.CreateConnection())
             {
                 conn.Open();
 
                 Database.LogStagesInfo(conn, data);
+                StagesInfoRecord[] records = GetStagesInfo(data);
 
-                StagesInfoRecord[] records = null;
-                try
-                {
-                    records = JsonConvert.DeserializeObject<StagesInfoRecord[]>(data);
-                }
-                catch { }
-
-                if (records == null || records.Length == 0) return;
+                if (records == null || records.Length == 0) return records;
 
                 {
                     StagesInfoRecord record = records[0];
@@ -94,17 +143,34 @@ namespace splatcrawler
                 for (int x = 1; x < records.Length; x++)
                 {
                     bool success = Database.InsertLeaderboard(conn, records[x]);
-                    if (success) Console.WriteLine("Inserted leaderboard for {0} to {1}.", records[x].datetime_term_begin, records[x].datetime_term_end);
-                    else Console.WriteLine("Leaderboard for {0} to {1} already in database.", records[x].datetime_term_begin, records[x].datetime_term_end);
+                    if (success)
+                    {
+                        if (pollType == PollTypes.Fresh) Console.WriteLine();
+                        Console.WriteLine("Inserted leaderboard for {0} to {1}.", records[x].datetime_term_begin, records[x].datetime_term_end);
+                    }
+                    else if (pollType != PollTypes.Fresh) Console.WriteLine("Already have leaderboard for {0} to {1}.", records[x].datetime_term_begin, records[x].datetime_term_end);
                 }
 
                 conn.Close();
+                return records;
             }
         }
 
+        public static DateTime TokyoToUtc(DateTime date)
+        {
+            TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+            return TimeZoneInfo.ConvertTimeToUtc(date);
+        }
 
         protected override void OnStop()
         {
         }
+    }
+
+    enum PollTypes
+    {
+        Initial,
+        Ambient,
+        Fresh,
     }
 }
