282f5939b9d444595997cbdd06c298d046538b83
Greg Edwards <edwardsgreg@gmail.com>
2015-07-25 19:32:28 -0400

    Hide navigation which only contains unimplemented things.

--- squidtracker/Main.master.designer.cs
+++ squidtracker/Main.master.designer.cs
@@ -31,6 +31,15 @@ namespace SquidTracker.Web {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder cpMain;
         
         /// <summary>
+        /// phNavHide control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder phNavHide;
+        
+        /// <summary>
         /// hlMain control.
         /// </summary>
         /// <remarks>
